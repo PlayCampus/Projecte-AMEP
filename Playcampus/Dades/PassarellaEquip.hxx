002fd5b99bed32e08a5eb0a3fabec759ee9ff49f
2025-ma <maria.fernanda.marquez@estudiantat.upc.edu>
2026-05-28 10:56:01 +0200

    tasca#216-Modificación Pasarela Equip y creación Pasarela EquipTemporada)-(Tarea no finalizada)

--- Playcampus/Dades/PassarellaEquip.hxx
+++ Playcampus/Dades/PassarellaEquip.hxx
@@ -10,53 +10,20 @@ namespace Playcampus {
         private:
             String^ connectionString;
             String^ idEquip;
-            String^ idTemporada;
             String^ nom;
             DateTime dataFundacio;
             String^ esport;
-            unsigned int partitsJugats;
-            unsigned int victories;
-            unsigned int derrotes;
-            unsigned int empats;
-            unsigned int punts;
-            unsigned int golsAFavor;
-            unsigned int golsEnContra;
-            int diferenciaGols;
-            int posicioClassificacio;
+      
             
 
         public:
             PassarellaEquip(String^ connStr);
             PassarellaEquip(String^ connStr, String^ idEquip, String^ nom, DateTime dataFundacio, String^ esport);
 
-            PassarellaEquip(String^ connStr, String^ idEquip, String^ nom, DateTime dataFundacio, String^ esport, unsigned int partitsJugats, unsigned int victories, unsigned int derrotes, unsigned int empats, unsigned int punts, unsigned int golsAFavor, unsigned int golsEnContra, int diferenciaGols, int posicioClassificacio, String^ idTemporada);
-
             String^ GetIdEquip();
             String^ GetNom();
             DateTime GetDataFundacio();
             String^ GetEsport();
-
-            unsigned int GetPartitsJugats();
-            unsigned int GetVictories();
-            unsigned int GetDerrotes();
-            unsigned int GetEmpats();
-            unsigned int GetPunts();
-            unsigned int GetGolsAFavor();
-            unsigned int GetGolsEnContra();
-            int GetDiferenciaGols();
-            int GetPosicioClassificacio();
-            String^ GetIdTemporada();
-
-            void SetIdTemporada(String^ nouIdTemporada);
-            void SetPartitsJugats(unsigned int partits);
-            void SetVictories(unsigned int victories);
-            void SetDerrotes(unsigned int derrotes);
-            void SetEmpats(unsigned int empats);
-            void SetPunts(unsigned int punts);
-            void SetGolsAFavor(unsigned int golsAFavor);
-            void SetGolsEnContra(unsigned int golsEnContra);
-            void SetDiferenciaGols(int diferencia);
-            void SetPosicioClassificacio(int posicio);
 
             void Insereix();
             void Modifica();
